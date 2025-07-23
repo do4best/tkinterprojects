617f8591764f003014f1b17133b34c64e6126db2
do4best657 <panic_4_what@hotmail.com>
2025-07-23 19:23:46 +0500

    Express js send and recive items were exercised

--- javascript/myexpress.js
+++ javascript/myexpress.js
@@ -1,11 +1,7 @@
-const express = require('express')
+import express from 'express'
 const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/",(req,res)=>{
+  console.log("Here")
+  res.sendStatus(500)
 })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(3000)
